094c39a55296f630e64de1ef089dee3efa573e8e
Dan Mack <mack@macktronics.com>
2016-09-25 21:23:43 -0500

    Initial commit
    Signed-off-by: Dan Mack <mack@macktronics.com>

---
+++ README.md
@@ -0,0 +1,10 @@
+# Generate a visual bitmap from the KISS pRNG
+
+![Random Number Bitmap](img/static.png "Visual Analysis of pRNG")
+
+Part two of my coding adventure with the Rust language.  This time,
+I write the KISS pseudo random number generator function and create
+a bitmap of it's output to see if there are any patterns.
+
+There are probably bugs as always so feel free to send me a PR.
+
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+//! generate bitmap of a stream of random numbers from an
+//! implementation of the KISS() pseudo random number generator.
+//!
+//! demonstrations how to use static/global variables in rust, bitwise
+//! operations, and the image crate.
+
+extern crate image;
+
+use std::fs::File;
+use std::path::Path;
+
+/// Multiply with carry (MWC) component
+pub static mut X: usize = 1234567890987654321;
+
+/// XOR Shift (XSH) - left 13, right 17, left 43
+pub static mut Y: usize = 362436362436362436;
+
+/// Congruential number generator (CNG) seed
+pub static mut Z: usize = 1066149217761810;
+pub static mut C: usize = 123456123456123456;
+
+/// width of the png image
+pub const WIDTH:  u32 = 700;
+
+/// height of the png image
+pub const HEIGHT: u32 = 700;
+
+fn main() {
+    // fill the imagebuffer with a blank slate, except 0,0
+    let mut img = image::ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
+        if x == 0 && y == 0 {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+
+    let pixel = img[(0,0)];
+    for x in 0 .. WIDTH {
+        for y in 0 .. HEIGHT {
+            if kiss() % 2 == 0 {
+                img.put_pixel(x, y, pixel);
+            }
+        }
+    }
+
+    let ref mut fout = File::create(&Path::new("static.png")).unwrap();
+    let _ = image::ImageLuma8(img).save(fout, image::PNG);
+}
+
+/// my attempt at implementing a 64-bit version of the KISS pseudo
+/// random number generator from Marsaglia and Zaman, Florida State U
+/// "Even a bad RNG is pretty good"
+/// For some strange reason, there isn't a good article for this on
+/// wikipedia in english - but I used this reference from the German
+/// version of wikipedia:
+/// [KISS Algorithm](https://de.wikipedia.org/wiki/KISS_(Zufallszahlengenerator)
+pub fn kiss() -> usize {
+    unsafe {
+        Z = 6906969069 * Z + 1234567;
+        Y ^= Y<<13;
+        Y ^= Y>>17;
+        Y ^= Y<<43;
+
+        let t: usize = (X<<58)+C;
+        C =  X>>6;
+        X += t;
+        C += X<<t;
+        X + Y + Z
+    }
+}
